adc542f4209254efb05e57ff17300c02cc27a5ec
Vadim <h4998998482@gmail.com>
2022-10-18 20:25:58 +0300

    start commit

---
+++ src/main/java/topjava/quest/graduationproject/GraduationProjectApplication.java
@@ -0,0 +1,15 @@
+package topjava.quest.graduationproject;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@AllArgsConstructor
+public class GraduationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationProjectApplication.class, args);
+    }
+
+}
